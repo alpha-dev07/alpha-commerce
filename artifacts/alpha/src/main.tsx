c5e4d5c6695bea03b7441f269eede689bcf054d2
alpha-dev07 <07alphabro@gmail.com>
2026-07-23 20:17:55 +0530

    Update main.tsx

--- artifacts/alpha/src/main.tsx
+++ artifacts/alpha/src/main.tsx
@@ -6,7 +6,7 @@ import OneSignal from "react-onesignal";
 const ONESIGNAL_APP_ID = "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83";
 
 OneSignal.init({
-  appId: ONESIGNAL_APP_ID,
+  appId: "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83",
 });
 
 createRoot(document.getElementById("root")!).render(<App />);
