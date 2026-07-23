d587fb2870e77184df219b0c16f37b22dfe9076a
alpha-dev07 <07alphabro@gmail.com>
2026-07-23 19:44:12 +0530

    Add OneSignal push notifications

--- artifacts/alpha/src/main.tsx
+++ artifacts/alpha/src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import "./index.css";
 import OneSignal from "react-onesignal";
 
+<<<<<<< HEAD
 async function initOneSignal() {
   await OneSignal.init({
     appId: "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83",
@@ -13,3 +14,12 @@ async function initOneSignal() {
 }
 
 initOneSignal();
+=======
+const ONESIGNAL_APP_ID = "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83";
+
+OneSignal.init({
+  appId: ONESIGNAL_APP_ID,
+});
+
+createRoot(document.getElementById("root")!).render(<App />);
+>>>>>>> 9e217eb (Add OneSignal push notifications)
