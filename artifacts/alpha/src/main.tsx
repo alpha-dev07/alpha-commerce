17add4f438dee1051df132285d32f0c8fa513a8e
alpha-dev07 <07alphabro@gmail.com>
2026-07-23 20:43:39 +0530

    Update main.tsx

--- artifacts/alpha/src/main.tsx
+++ artifacts/alpha/src/main.tsx
@@ -3,10 +3,13 @@ import App from "./App";
 import "./index.css";
 import OneSignal from "react-onesignal";
 
-const ONESIGNAL_APP_ID = "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83";
+async function initOneSignal() {
+  await OneSignal.init({
+    appId: "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83",
+    allowLocalhostAsSecureOrigin: true,
+  });
 
-OneSignal.init({
-  appId: "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83",
-});
+  createRoot(document.getElementById("root")!).render(<App />);
+}
 
-createRoot(document.getElementById("root")!).render(<App />);
+initOneSignal();
