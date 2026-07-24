f776fbe94968ce3c011cf65bfcad53bb8bb91bff
alpha-dev07 <07alphabro@gmail.com>
2026-07-24 19:18:18 +0530

    Update main.tsx

--- artifacts/alpha/src/main.tsx
+++ artifacts/alpha/src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import OneSignal from "react-onesignal";
 import App from "./App";
+import "./index.css";
 
 async function start() {
   await OneSignal.init({
     appId: "6aa9d9b0-5f97-4b5a-be06-c700d7e63c83",
     serviceWorkerPath: "/OneSignalSDKWorker.js",
     serviceWorkerParam: { scope: "/" },
-    notifyButton: { enable: false },
+    notifyButton: {
+      enable: false,
+    },
   });
 
   OneSignal.Debug.setLogLevel("trace");
